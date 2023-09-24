be1dd06b30e4cf5bd13dde0e7dc0d77eff6ba2d0
Pavel Mamilov <pmamilov@ya.ru>
2023-09-24 12:51:36 +0300

    the display template is configured for different resolutions

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [isCopy, setIsCopy] = useState(false);
 
   const clipboardHandle = () => {
-    if(!isCopy) {
+    if (!isCopy) {
       copy("REFBNK8Q");
       setIsCopy(true);
       //ym('000','reachGoal','copy')
@@ -26,15 +26,14 @@ function App() {
         <div className="container">
           <div className="head">
             <h1>
-              Промокод&nbsp;на 1000&nbsp;рублей в&nbsp;приложении Делимобиля
+              Промокод&nbsp;на 1000&nbsp;рублей в&nbsp;приложении Делимобиля{" "}
             </h1>
           </div>
           <div className="content">
             <div className="promo">
-              <span>REFBNK8Q</span>
+              <span onClick={clipboardHandle}>REFBNK8Q</span>
             </div>
             <div className="copy">
-              <div className="tooltip">
                 <span
                   type="button"
                   name="button"
@@ -42,43 +41,46 @@ function App() {
                   onMouseOut={changeTooltip}
                   className="button"
                 >
-                {!isCopy ? "скопировать" : "получилось!"}  
+                  {!isCopy ? "скопировать" : "получилось!"}
                 </span>
-              </div>
             </div>
           </div>
           <div className="main-text">
             <p>
-              Вам зачислится 1000 рублей в&nbsp;приложении Делимобиля после ввода&nbsp;промокода.
-              Вы&nbsp;платите только&nbsp;50% от&nbsp;стоимости поездки! 
-              Новым клиентам.
+              Вам зачислится 1000 рублей в&nbsp;приложении Делимобиля после
+              ввода&nbsp;промокода. Вы&nbsp;платите только&nbsp;50%
+              от&nbsp;стоимости поездки! Новым клиентам.
             </p>
             <p>
               <span className="attention">Внимание! </span>
               Бонусы зачисляются не&nbsp;сразу, дело в&nbsp;том, что Делимобиль
-              около часа проверяет промокод, поэтому нужно подождать, иначе
-              не&nbsp;сработает!
+              около часа проверяет промокод, поэтому нужно подождать{" "}
+              <img src="1000r.jpg" width="60px" />, иначе не&nbsp;сработает!
+            </p>
+            <p>
+              <span className="now">Теперь:</span> выберите операционную систему
+              вашего телефона и&nbsp;скачайте приложение Делимобиля.
             </p>
           </div>
           <div className="download">
-            <div className="android">
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href="https://play.google.com/store/apps/details?id=com.carshering&hl=ru&gl=US"
-                className="buttons"
-              >
-                android
-              </a>
-            </div>
             <div className="ios">
               <a
                 target="_blank"
                 rel="noreferrer"
                 href="https://apps.apple.com/ru/app/делимобиль-твой-каршеринг/id1038254296"
+                className="buttons radius"
+              >
+                &nbsp;iOS
+              </a>
+            </div>
+            <div className="android">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://play.google.com/store/apps/details?id=com.carshering"
                 className="buttons"
               >
-                iOS
+                &#128241;&nbsp;android
               </a>
             </div>
           </div>
